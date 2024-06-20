1d272efb0b27a69af603239f81d39e380448ad5c
Yadao08 <90585871+Yadao08@users.noreply.github.com>
2024-06-20 10:36:09 +0800

    Update sbc-d2-act2.py

--- sbc-d2-act2.py
+++ sbc-d2-act2.py
@@ -1,38 +1,29 @@
 
-'''HUMPYANG GAME
-P1 = YOU (REAL PLAYER)
-C2,C3 = (COMPUTER)
-P1 = kulob - USER INPUT,
-C2 = hayang - RANDOM,
-C3 = kulob -  RANDOM '''
-
 from random import randint
 
-ako = input("type hayang or kulob: ")
-Com2 = randint(0,1)
-Com3 = randint(0,1)
+P1 = int(input("enter 0 hayang to  1 kulob: "))
+c2 = randint(0,1)
+c3 = randint(0,1)
 
-kulob = 1
-hayang = 0
+if P1 == 1:
+    print("P1:kulob")
+elif P1 == 0:
+    print("P1:hayang")
+if c2 == 1:
+    print("C2:kulob")
+elif c2 == 0:
+    print("C2:hayang")
+if c3 == 1:
+    print("C3:kulob")
+elif c3 == 0:
+    print("C3:hayang")
 
-if ako == kulob:
-    print("ako:kulob")
-elif ako == hayang:
-    print("ako:hayang")
-if Com2 == kulob:
-    print("Com2:kulob")
-elif Com2 == hayang:
-    print("Com2:hayang")
-if Com3 == kulob:
-    print("Com3:kulob")
-elif Com3 == hayang:
-    print("Com3:hayang")
 
-if ako == hayang and Com2 == kulob and Com3 == kulob or ako == kulob and Com2 == hayang and Com3 == hayang :
+if P1 == 0 and c2 == 1 and c3 == 1 or P1 == 1 and c2 == 0 and c3 == 0 :
     print("you win")
-elif ako == kulob and Com2 == hayang and Com3 == kulob or ako == hayang and Com2 == kulob and Com3 == hayang:
+elif P1 == 1 and c2 == 0 and c3 == 1 or P1 == 0 and c2 == 1 and c3 == 0:
     print("c2 win")
-elif ako == kulob and Com2 == kulob and Com3 == hayang or ako == hayang and Com2 == hayang and Com3 == kulob:
-    print("c2 win")
+elif P1 == 1 and c2 == 1 and c3 == 0 or P1 == 0 and c2 == 0 and c3 == 1:
+    print("c3 win")
 else:
     print("draw")
